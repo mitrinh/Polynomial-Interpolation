efa0281d9bb28a93a73e8f3096130417fd752cd4
Michael Trinh <mptrinh@cpp.edu>
2018-11-22 22:37:20 -0800

    added base code

---
+++ PolynomialInterpolation.java
@@ -0,0 +1,23 @@
+/**
+ * Michael Trinh
+ * CS 3010
+ * Project 3
+ * Description: Creates a divided difference table from the given data in a text
+ *  file and uses that to create the interpolating polynomial. Prints the 
+ *  polynomial in both the Newton's form and Lagrange's form.
+ */
+
+/**
+ *
+ * @author Michael Trinh
+ */
+public class PolynomialInterpolation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Michael Trinh's Polynomial Interpolation");
+    }
+    
+}
